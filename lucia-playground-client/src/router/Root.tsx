1d9178ef021d467375765dd8b5689824057e52b4
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-12-11 20:33:01 +0100

    Add source code link

--- lucia-playground-client/src/router/Root.tsx
+++ lucia-playground-client/src/router/Root.tsx
@@ -34,6 +34,21 @@ export function Root() {
         </header>
 
         <Outlet />
+
+        <footer>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="https://github.com/wasp-lang/lucia-playground/tree/lucia-v3"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://img.shields.io/badge/Source%20Code-Lucia%20Playground-black?logo=github"
+                alt="Source Code"
+              />
+            </a>
+          </div>
+        </footer>
       </div>
     </>
   );
